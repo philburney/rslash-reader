72093a1527459f45d948c174b3688c3d873fc04d
Phil Burney-Cumming <argy@argys.co.uk>
2022-08-17 16:14:04 +0100

    search clears and disables

--- src/components/Search.js
+++ src/components/Search.js
@@ -12,17 +12,21 @@ export const Search = () => {
     const dispatch = useDispatch();
 
     const [searchTerm,setSearchTerm] = useState("");
+ 
 
     const currentTitle = useSelector(selectTitle);
     const currentUrl = useSelector(URL);
-
+  
     const handleSearchChange = (e) => {
         setSearchTerm(e.target.value);
     };
+    
    
     const handleSubmitSearch = (e) => {
         e.preventDefault();
         const newTitle = `Seaching for ${searchTerm} in ${currentTitle}` ;
+
+        
         let newURL = currentUrl.substring(0,currentUrl.length-5) + `/search.json?q=${searchTerm}`;
         
         console.log(searchTerm);
@@ -32,6 +36,7 @@ export const Search = () => {
         console.log(newTitle);
         console.log(newURL);
         dispatch(setSectionTitleAndURL({newTitle,newURL}));
+        setSearchTerm("");
         
         
     }
@@ -39,8 +44,8 @@ export const Search = () => {
 
     return (
         <form className="search">
-            <input type="text" className="searchbox" placeholder="Search" onChange={handleSearchChange}/>
-            <button onClick={handleSubmitSearch}  >Search</button>
+            <input type="text" className="searchbox" placeholder="Search" value={searchTerm} onChange={handleSearchChange}/>
+            <button onClick={handleSubmitSearch}  disabled={!searchTerm} >Search</button>
            
        </form>
     )
